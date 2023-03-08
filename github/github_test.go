9ec1b5a843c4a927042343ba6bba6d056f6a7b99
Geraldo Castro <victormatheuscastro@gmail.com>
2023-03-08 13:30:36 -0300

    WIP - Change testJSONMarshal and some test cases

--- github/github_test.go
+++ github/github_test.go
@@ -6,6 +6,7 @@
 package github
 
 import (
+	"bytes"
 	"context"
 	"encoding/json"
 	"errors"
@@ -147,25 +148,14 @@ func testBody(t *testing.T, r *http.Request, want string) {
 // to the want string.
 func testJSONMarshal(t *testing.T, v interface{}, want string) {
 	t.Helper()
-	// Unmarshal the wanted JSON, to verify its correctness, and marshal it back
-	// to sort the keys.
-	u := reflect.New(reflect.TypeOf(v)).Interface()
-	if err := json.Unmarshal([]byte(want), &u); err != nil {
-		t.Errorf("Unable to unmarshal JSON for %v: %v", want, err)
-	}
-	w, err := json.Marshal(u)
-	if err != nil {
-		t.Errorf("Unable to marshal JSON for %#v", u)
-	}
 
-	// Marshal the target value.
 	j, err := json.Marshal(v)
 	if err != nil {
-		t.Errorf("Unable to marshal JSON for %#v", v)
+		t.Errorf("Unable to unmarshal JSON for %v: %v", v, err)
 	}
 
-	if string(w) != string(j) {
-		t.Errorf("json.Marshal(%q) returned %s, want %s", v, j, w)
+	if !bytes.Equal(j, []byte(want)) {
+		t.Errorf("json.Marshal(%+v) returned %s, want %s", v, j, want)
 	}
 }
 
